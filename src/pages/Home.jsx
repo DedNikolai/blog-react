c2b1663b6ebc21fc0fdb26be5c96661fae7cbc25
unknown <nikolai.blashchuk@gmail.com>
2024-10-24 12:56:42 +0300

    deploy app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useContext } from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Grid from '@mui/material/Grid';
